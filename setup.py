2d6d0e65006674154a0f2b566bb09377c137b1f1
Noah Altunian <noah@cirruslabs.io>
2019-02-24 23:19:04 -0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='phort',
+    description='The Python + GNU/Linux Tor router CLI',
+    url='https://github.com/naltun/phort',
+    author='Noah Altunian',
+    author_email='noah@cirruslabs.io',
+    license='Mozilla Public License 2.0',
+    packages=['phort'],
+    zip_safe=True
+)
